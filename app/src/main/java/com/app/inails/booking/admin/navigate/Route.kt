4b63bcbe1a6606da90cfa6a764f874a354a0f969
Yen Yen <lkimlyen2912@gmail.com>
2022-09-09 18:24:24 +0700

    update router booking

--- app/src/main/java/com/app/inails/booking/admin/navigate/Route.kt
+++ app/src/main/java/com/app/inails/booking/admin/navigate/Route.kt
@@ -1,10 +1,40 @@
 package com.app.inails.booking.admin.navigate
+import android.support.core.route.BundleArgument
+import android.support.core.route.RouteDispatcher
+import android.support.core.route.open
+import android.support.navigation.NavOptions
+import android.support.navigation.findNavigator
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentActivity
+import com.app.inails.booking.admin.R
+import com.app.inails.booking.admin.views.main.MainNavigationActivity
+import kotlinx.parcelize.Parcelize
+import kotlin.reflect.KClass
 
-interface Route : SplashRoute,SettingRoute,MainNavigateRoute {
-		companion object : Route by DevRoute()
+
+interface Routing : BundleArgument {
+    val fragmentClass: KClass<out Fragment>
+    val options: NavOptions? get() = null
 }
 
-open class DevRoute : Route,
-		SplashRoute by SplashRouteImpl(),
-		SettingRoute by SettingRouteImpl(),
-		MainNavigateRoute by MainNavigateRouteImpl()
+interface Router :  BookingRoute {
+    fun open(dispatcher: RouteDispatcher, route: Routing)
+    fun navigate(dispatcher: RouteDispatcher, route: Routing)
+    companion object : Router by ProdRoute()
+}
+
+class ProdRoute : Router,
+    BookingRoute by BookingRouteImpl() {
+    override fun open(dispatcher: RouteDispatcher, route: Routing) {
+        dispatcher.open<MainNavigationActivity>(route)
+    }
+
+    override fun navigate(dispatcher: RouteDispatcher, route: Routing) {
+        val navigator = when (dispatcher) {
+            is FragmentActivity -> dispatcher.findNavigator()
+            is Fragment -> dispatcher.findNavigator()
+            else -> error("Not found navigator")
+        }
+        navigator.navigate(route.fragmentClass, route.toBundle(), route.options)
+    }
+}
